5b3690171e057168c7b81a65c1d430c4eb29b5b8
SimonN <65023744+Simon994@users.noreply.github.com>
2020-10-20 18:28:27 +0100

    started new Django project, and created new photos app

---
+++ photos/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Photo(models.Model):
+    title = models.CharField(max_length = 75)
+    description = models.TextField(max_length=300, blank=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    taken_at = models.DateTimeField(null=True, blank=True)
+    camera = models.CharField(max_length = 50, blank=True)
+    lens = models.CharField(max_length = 50, blank=True)
+    image = models.CharField(max_length=400)
+    location = models.CharField(max_length = 75)
