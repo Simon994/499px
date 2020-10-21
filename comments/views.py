a8f1c93da7420b31270d8dbc9fabc7ec093f877c
SimonN <65023744+Simon994@users.noreply.github.com>
2020-10-21 16:28:54 +0100

    added delete to CommentDetailView

--- comments/views.py
+++ comments/views.py
@@ -3,10 +3,11 @@ from django.shortcuts import render
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
 from rest_framework import status
 
 from .serializers.common import CommentSerializer
-# from .models import Comment
+from .models import Comment
 
 class CommentListView(APIView):
 
@@ -16,3 +17,17 @@ class CommentListView(APIView):
             comment_to_create.save()
             return Response(comment_to_create.data, status=status.HTTP_201_CREATED)
         return Response(comment_to_create.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+
+class CommentDetailView(APIView):
+
+    def get_comment(self, pk):
+        try:
+            return Comment.objects.get(pk=pk)
+        except Comment.DoesNotExist:
+            raise NotFound()   
+
+    def delete(self, _request, pk):
+        comment_to_delete = self.get_comment(pk=pk)
+        comment_to_delete.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- photos/serializers/populated.py
+++ photos/serializers/populated.py
@@ -1,6 +1,7 @@
-from ..serializers.common import PhotoSerializer
+# pylint: disable=no-name-in-module, import-error
 from comments.serializers.common import CommentSerializer
+from ..serializers.common import PhotoSerializer
 
 class PopulatedPhotoSerializer(PhotoSerializer):
-    
+
     comments = CommentSerializer(many=True)
