b3c9c11d0dc2c0f9a544b6bb895f69e60d3cb6c8
SimonN <65023744+Simon994@users.noreply.github.com>
2020-10-21 12:24:37 +0100

    PhotoDetailView added, with get (show single photo), and post (create photo) added to PhotoListView

--- photos/urls.py
+++ photos/urls.py
@@ -1,7 +1,8 @@
 # pylint: disable=no-name-in-module, import-error
 from django.urls import path
-from .views import PhotoListView
+from .views import PhotoListView, PhotoDetailView
 
 urlpatterns = [
-    path('', PhotoListView.as_view())
+    path('', PhotoListView.as_view()),
+    path('<int:pk>/', PhotoDetailView.as_view())
 ]
--- photos/views.py
+++ photos/views.py
@@ -1,7 +1,8 @@
 # pylint: disable=no-name-in-module, import-error
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework.exceptions import NotFound
 
 from .models import Photo
 from .serializers.common import PhotoSerializer
@@ -12,5 +13,24 @@ class PhotoListView(APIView):
         photos_list = Photo.objects.all()
         serialized_photo_list = PhotoSerializer(photos_list, many=True)
         return Response(serialized_photo_list.data, status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        photo_to_create = PhotoSerializer(data=request.data)
+        if photo_to_create.is_valid():
+            photo_to_create.save()
+            return Response(photo_to_create.data, status=status.HTTP_201_CREATED)
+        return Response(photo_to_create.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
 
 
+class PhotoDetailView(APIView):
+
+    def get_photo(self, pk):
+        try:
+            return Photo.objects.get(pk=pk)
+        except Photo.DoesNotExist:
+            raise NotFound()
+
+    def get(self, _request, pk):
+        photo = self.get_photo(pk=pk)
+        serialized_single_photo = PhotoSerializer(photo)
+        return Response(serialized_single_photo.data, status=status.HTTP_200_OK)
