b435ba8d1cb0b5d379304e0fefb9837706edff67
SimonN <65023744+Simon994@users.noreply.github.com>
2020-10-21 19:43:44 +0100

    permission classes added in comment views, and function to allow comment deletion only if user is comment owner

--- comments/views.py
+++ comments/views.py
@@ -1,17 +1,17 @@
-from django.shortcuts import render
-
-
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.exceptions import NotFound
+from rest_framework.exceptions import NotFound, PermissionDenied
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 
 from .serializers.common import CommentSerializer
 from .models import Comment
 
 class CommentListView(APIView):
+    permission_classes = (IsAuthenticated, )
 
     def post(self, request):
+        request.data['owner'] = request.user.id
         comment_to_create = CommentSerializer(data=request.data)
         if comment_to_create.is_valid():
             comment_to_create.save()
@@ -20,14 +20,20 @@ class CommentListView(APIView):
 
 
 class CommentDetailView(APIView):
+    permission_classes = (IsAuthenticated, )
 
     def get_comment(self, pk):
         try:
             return Comment.objects.get(pk=pk)
         except Comment.DoesNotExist:
-            raise NotFound()   
+            raise NotFound()
 
-    def delete(self, _request, pk):
+    def is_comment_owner(self, comment , user):
+        if comment.owner.id != user.id:
+            raise PermissionDenied()
+
+    def delete(self, request, pk):
         comment_to_delete = self.get_comment(pk=pk)
+        self.is_comment_owner(comment_to_delete, request.user)
         comment_to_delete.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
