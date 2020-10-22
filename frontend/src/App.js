54c94289091613d3dc3c43b790810e0851c9b8d8
SimonN <65023744+Simon994@users.noreply.github.com>
2020-10-22 18:06:26 +0100

    begining frontend, Home component added and starting routing

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import Home from './Components/common/Home'
+
+const App = () => {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={Home} />
+
+        </Switch>
+      </BrowserRouter>
+    </>
+  )
+
+}
+
+export default App
+
+
