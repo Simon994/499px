e50f2d8f2cabc2f156b8aa2df2b00ca0a3cf78b9
SimonN <65023744+Simon994@users.noreply.github.com>
2020-10-24 17:15:39 +0100

    styling updates for home page

--- frontend/src/Components/common/Home.js
+++ frontend/src/Components/common/Home.js
@@ -1,6 +1,6 @@
 
 import React from 'react'
-import { Button } from 'semantic-ui-react'
+import { Button, Icon} from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 
 const Home = () => {
@@ -8,22 +8,50 @@ const Home = () => {
   return (
     <>
       <div className='home-hero'>
+        
+        <div className='hero-text-container'>
+          <h1 className='hero-title'>Discover and share photos:<br /> a 500px clone</h1>
+          <p className='hero-sub'>This is a clone of the website 500px.</p>
+          <Button as={Link} to='/join'>Sign up</Button>
+        </div>
+        
         <svg id="layer-1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1416.99 174.01">
           <g fill="#FFF">
-            <path d="M0,280.8S283.66,57,608.94,163.56s437.93,150.75,808,10.34V309.54H0V280.8Z" transform="translate(0 -110.53)" />
+            <path d="M0,280.8S283.66,57,608.94,163.56s437.93,150.75,808,10.34V309.54H0V280.8Z" transform="translate(0 -135.53)" />
           </g>
         </svg>
-
+        <p id='hero-image-credit'>Photo by Patrick Fore on Unsplash</p>
       </div>
-      <section>
-        {/* <svg id="Layer_1" data-name="Layer 1" xmlns="http://
-        www.w3.org/2000/svg" viewBox="0 0 1416.99 174.01"><defs><style>
-            .cls-1{{ fill: '#74c044' }}</style></defs><title>Untitled-4</title>
-          <path className="cls-1" d="M0,280.8S283.66,57,608.94,163.56s437.93,150.75,808,10.34V309.54H0V280.8Z"
-            transform="translate(0 -135.53)" />
-        </svg> */}
-      </section>
-      <Button as={Link} to='/join'>Sign up</Button>
+
+      <div className='home-details'>
+        <h1>What makes us the same</h1>
+        <div className='home-details-text'>
+          <div className='detail-tile'>
+            <Icon name='pencil' size='huge'/>
+            <h3>Style and feel</h3>
+            <p>I am tring to emulate as much as possible
+              to the style and feel. 
+              This is an ongoing effort to
+              improve my skills with both React and Sass.
+            </p>
+          </div>
+          <div className='detail-tile'>
+            <Icon name='address card outline' size='huge'/>
+            <h3>Discover and post</h3>
+            <p>You can post photos, browse photos by others, and follow photographers.
+              Photos from those you follow will then appear in your home feed.
+            </p>
+          </div>
+          <div className='detail-tile'>
+            <Icon name='github' size='huge'/>
+            <h3>Take a look on github</h3>
+            <p>This is an ongoing project. Take a look at the code on <a href="https://github.com/Simon994/RAWShot" target='blank'>github</a>. 
+            </p>
+          </div>
+
+        </div>
+      </div>
+
     </>
   )
 
