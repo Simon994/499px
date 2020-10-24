f6d2b771e9b1794b12b50099976d0bad149102e9
SimonN <65023744+Simon994@users.noreply.github.com>
2020-10-24 19:54:45 +0100

    first pass done for styling of log in form

--- frontend/src/Components/auth/Login.js
+++ frontend/src/Components/auth/Login.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Button, Form, Grid, Header, Message } from 'semantic-ui-react'
-import { Redirect } from 'react-router-dom'
+import { Button, Form, Icon, Message } from 'semantic-ui-react'
+import { Redirect, Link } from 'react-router-dom'
 
 import { loginUser, getUserProfile } from '../../lib/api'
 import { setToken } from '../../lib/auth'
@@ -20,9 +20,10 @@ class Login extends React.Component {
       ...this.state.formData,
       [e.target.name]: e.target.value
     }
-    this.setState({ 
-      formData, 
-      formUsernameError: false })
+    this.setState({
+      formData,
+      formUsernameError: false
+    })
   }
 
 
@@ -57,40 +58,55 @@ class Login extends React.Component {
     return (
       <>
 
-        <div id='shadow-wrapper' >
-          <Grid textAlign='center' style={{ height: '50vh' }} verticalAlign='middle'>
-            <Grid.Column style={{ maxWidth: 450 }} id='auth-column'>
-              <Header as='h2' color='black' textAlign='center'>
-                Log-in to your account
-              </Header>
-              <div className='ui container small'>
-                <Form onSubmit={this.handleSubmit} error={this.state.formUsernameError}>
-                  {this.state.formUsernameError ? (
-                    <Message error header='Fail' content='Please enter your email and password' />
-                  ) : null}
-                  <Form.Field>
-                    <label className='form-label'>Email</label>
-                    <input placeholder='email'
-                      onChange={this.handleChange}
-                      value={email}
-                      name='email'
-                    />
-                  </Form.Field>
-                  <Form.Field>
-                    <label className='form-label'>Password</label>
-                    <input placeholder='password'
-                      onChange={this.handleChange}
-                      value={password}
-                      name='password'
-                    />
-                  </Form.Field>
-                  <Button type='submit'>Log in</Button>
-                </Form>
-              </div>
-            </Grid.Column>
-          </Grid>
+        <div className='auth-form-container' >
+          <div className='auth-container'>
+            <Form onSubmit={this.handleSubmit} error={this.state.formUsernameError}>
+              {this.state.formUsernameError ? (
+                <Message error header='Fail' content='Please enter your email and password' />
+              ) : null}
+              <h2>Log in to 499px</h2>
+              <Form.Field>
+                <label className='form-label'>Email</label>
+                <input placeholder='email'
+                  onChange={this.handleChange}
+                  value={email}
+                  name='email'
+                />
+              </Form.Field>
+              <Form.Field>
+                <label className='form-label'>Password</label>
+                <input placeholder='password'
+                  onChange={this.handleChange}
+                  value={password}
+                  name='password'
+                />
+              </Form.Field>
+              <Button className='lozenge login-email-btn' type='submit'>Log in</Button>
+
+              <Button className='lozenge login-fb-btn' type='submit' animated='fade'>
+                <Button.Content visible>
+                  <Icon name='facebook official' />
+                Log in with Facebook
+                </Button.Content>
+                <Button.Content hidden>
+                  <Icon color='red' name='warning' />
+                  Coming soon</Button.Content>
+              </Button>
+
+              <Button className='lozenge login-google-btn' type='submit' animated='fade'>
+                <Button.Content visible>
+                  <Icon name='google' />
+                Log in with Google
+                </Button.Content>
+                <Button.Content hidden>
+                  <Icon color='red' name='warning' />
+                  Coming soon</Button.Content>
+              </Button>
+            </Form>
+            <p>Don't have an account? <Link to='/signup'>Sign up</Link></p>
+          </div>
         </div>
-        
+
       </>
     )
   }
