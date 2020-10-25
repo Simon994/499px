d510566386c78a04255433522e6cd355fe5d971a
SimonN <65023744+Simon994@users.noreply.github.com>
2020-10-25 15:52:55 +0000

    styling PhotoSubmit

--- frontend/src/Components/photos/PhotoSubmit.js
+++ frontend/src/Components/photos/PhotoSubmit.js
@@ -54,15 +54,15 @@ class PhotoSubmit extends React.Component {
         },
         redirect: '/photoshome'
       })
-    } catch (err){
+    } catch (err) {
       console.error('ERR in photo upload', err.response.data)
     }
   }
 
   render() {
 
-    if (this.state.redirect){
-      return <Redirect to={this.state.redirect}/>
+    if (this.state.redirect) {
+      return <Redirect to={this.state.redirect} />
     }
 
     if (!this.state.formData.image) return <h1>Just getting that for you</h1>
@@ -70,38 +70,47 @@ class PhotoSubmit extends React.Component {
     const { image, title, description, location } = this.state.formData
 
     return (
-      <div className='photo-submit-outer'>
-        <div className='image-upload-container'>
-          <img src={image} />
+      <>
+        <h3 className='upload-header'>Upload</h3>
+        <div className='photo-submit-outer'>
+          <div className='image-upload-container'>
+            <img src={image} />
+          </div>
+          <div className='photo-form-container'>
+            <Form onSubmit={this.handleSubmit}>
+              <Form.Field>
+                <label className='form-label'>Title</label>
+                <input placeholder='Enter Title'
+                  onChange={this.handleChange}
+                  value={title}
+                  name='title'
+                />
+              </Form.Field>
+              <Form.Field>
+                <label className='form-label'>Description</label>
+                <textarea placeholder='e.g. High angle view of a couple, looking down over a wide valley'
+                  onChange={this.handleChange}
+                  value={description}
+                  name='description'
+                />
+              </Form.Field>
+              <Form.Field>
+                <label className='form-label'>Location</label>
+                <input placeholder='Enter Location'
+                  onChange={this.handleChange}
+                  value={location}
+                  name='location'
+                />
+              </Form.Field>
+              <div className='upload-btns-container'>
+                <Button className='lozenge'>Upload</Button>
+
+              </div>
+              
+            </Form>
+          </div>
         </div>
-        <Form onSubmit={this.handleSubmit}>
-          <Form.Field>
-            <label className='form-label'>Title</label>
-            <input placeholder='Enter Title'
-              onChange={this.handleChange}
-              value={title}
-              name='title'
-            />
-          </Form.Field>
-          <Form.Field>
-            <label className='form-label'>Description</label>
-            <textarea placeholder='e.g. High angle view of a couple, looking down over a wide valley'
-              onChange={this.handleChange}
-              value={description}
-              name='description'
-            />
-          </Form.Field>
-          <Form.Field>
-            <label className='form-label'>Location</label>
-            <input placeholder='Enter Location'
-              onChange={this.handleChange}
-              value={location}
-              name='location'
-            />
-          </Form.Field>
-          <Form.Field control={Button}>Upload</Form.Field>
-        </Form>
-      </div>
+      </>
     )
   }
 
