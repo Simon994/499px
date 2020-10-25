f16116f2f3fe75f602640566969ac5c7e8b6ff62
SimonN <65023744+Simon994@users.noreply.github.com>
2020-10-25 16:42:38 +0000

    adding react select to PhotoSubmit and further styling

--- frontend/src/Components/photos/PhotoSubmit.js
+++ frontend/src/Components/photos/PhotoSubmit.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Select from 'react-select'
+
 import { Form, Button } from 'semantic-ui-react'
 import { Redirect } from 'react-router-dom'
 
 import { createPhoto } from '../../lib/api'
+import { photoCategories } from './photosSubComponents/PhotoCategories'
 
 class PhotoSubmit extends React.Component {
 
@@ -69,6 +72,17 @@ class PhotoSubmit extends React.Component {
 
     const { image, title, description, location } = this.state.formData
 
+    const options = photoCategories.map(category => {
+      return { value: category.fields.name, label: category.fields.name }
+    })
+
+    console.log(options)
+    // [
+    //   { value: 'Abstract', label: 'Chocolate' },
+    //   { value: 'Aerial', label: 'Strawberry' },
+    //   { value: 'vanilla', label: 'Vanilla' }
+    // ]
+
     return (
       <>
         <h3 className='upload-header'>Upload</h3>
@@ -78,35 +92,37 @@ class PhotoSubmit extends React.Component {
           </div>
           <div className='photo-form-container'>
             <Form onSubmit={this.handleSubmit}>
-              <Form.Field>
-                <label className='form-label'>Title</label>
-                <input placeholder='Enter Title'
-                  onChange={this.handleChange}
-                  value={title}
-                  name='title'
-                />
-              </Form.Field>
-              <Form.Field>
-                <label className='form-label'>Description</label>
-                <textarea placeholder='e.g. High angle view of a couple, looking down over a wide valley'
-                  onChange={this.handleChange}
-                  value={description}
-                  name='description'
-                />
-              </Form.Field>
-              <Form.Field>
-                <label className='form-label'>Location</label>
-                <input placeholder='Enter Location'
-                  onChange={this.handleChange}
-                  value={location}
-                  name='location'
-                />
-              </Form.Field>
+              <div className='upload-form-inner-top'>
+                <Form.Field>
+                  <label className='form-label'>Title</label>
+                  <input placeholder='Enter Title'
+                    onChange={this.handleChange}
+                    value={title}
+                    name='title'
+                  />
+                </Form.Field>
+                <Form.Field>
+                  <label className='form-label'>Description</label>
+                  <textarea placeholder='e.g. High angle view of a couple, looking down over a wide valley'
+                    onChange={this.handleChange}
+                    value={description}
+                    name='description'
+                  />
+                </Form.Field>
+                <Form.Field>
+                  <label className='form-label'>Location</label>
+                  <input placeholder='Enter Location'
+                    onChange={this.handleChange}
+                    value={location}
+                    name='location'
+                  />
+                </Form.Field>
+                <Select options={options} isMulti />
+              </div>
               <div className='upload-btns-container'>
                 <Button className='lozenge'>Upload</Button>
-
               </div>
-              
+
             </Form>
           </div>
         </div>
---
+++ frontend/src/Components/photos/photosSubComponents/PhotoCategories.js
@@ -0,0 +1,52 @@
+
+export const photoCategories = [
+  {
+    model: 'photo_categories.photocategory',
+    pk: 2,
+    fields: {
+      name: 'Abstract'
+    }
+  },
+  {
+    model: 'photo_categories.photocategory',
+    pk: 3,
+    fields: {
+      name: 'Aerial'
+    }
+  },
+  {
+    model: 'photo_categories.photocategory',
+    pk: 4,
+    fields: {
+      name: 'Animals'
+    }
+  },
+  {
+    model: 'photo_categories.photocategory',
+    pk: 5,
+    fields: {
+      name: 'Celebrities'
+    }
+  },
+  {
+    model: 'photo_categories.photocategory',
+    pk: 6,
+    fields: {
+      name: 'City & Architecture'
+    }
+  },
+  {
+    model: 'photo_categories.photocategory',
+    pk: 7,
+    fields: {
+      name: 'Black and White'
+    }
+  },
+  {
+    model: 'photo_categories.photocategory',
+    pk: 8,
+    fields: {
+      name: 'Commercial'
+    }
+  }
+]
