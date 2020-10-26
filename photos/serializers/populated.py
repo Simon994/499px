35d459e303d44842dbfe9fe718f8d96dbd1b02d8
SimonN <65023744+Simon994@users.noreply.github.com>
2020-10-26 14:20:00 +0000

    updated photo populated serializer to include PopulatedCommentSerializer (for profile images and usernames)

--- photos/serializers/populated.py
+++ photos/serializers/populated.py
@@ -1,12 +1,13 @@
 # pylint: disable=no-name-in-module, import-error
 from jwt_auth.serializers.nested import NestedUserSerializer
 from comments.serializers.common import CommentSerializer
+from comments.serializers.populated import PopulatedCommentSerializer
 from photo_categories.serializers.common import PhotoCategorySerializer
 from ..serializers.common import PhotoSerializer
 
 class PopulatedPhotoSerializer(PhotoSerializer):
 
-    comments = CommentSerializer(many=True)
+    comments = PopulatedCommentSerializer(many=True)
     categories = PhotoCategorySerializer(many=True)
     owner = NestedUserSerializer()
     liked_by = NestedUserSerializer(many=True)
