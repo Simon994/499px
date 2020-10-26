687511894a46abf227ca5cd07ca7649eb108ec87
SimonN <65023744+Simon994@users.noreply.github.com>
2020-10-26 17:24:56 +0000

    styled comments box

--- frontend/src/Components/photos/PhotoShow.js
+++ frontend/src/Components/photos/PhotoShow.js
@@ -10,7 +10,8 @@ class PhotoShow extends React.Component {
   state = {
     singlePhotoData: null,
     formText: '',
-    postBtnHidden: true
+    postBtnHidden: true,
+    currentUserPhoto: null
   }
 
   async componentDidMount() {
@@ -23,11 +24,13 @@ class PhotoShow extends React.Component {
     const likedByArrayIds = response.data.liked_by.map(likedBy => likedBy.id)
     const isLikedByCurrentUser = likedByArrayIds.includes(currentUserId)
     const heartColor = isLikedByCurrentUser ? 'pink' : 'grey'
+    const currentUserPhoto = currentUserProfile.data.profile_image
 
     this.setState({
       singlePhotoData: response.data,
       heartColor,
-      liked: isLikedByCurrentUser
+      liked: isLikedByCurrentUser,
+      currentUserPhoto
     })
   }
 
@@ -88,7 +91,7 @@ class PhotoShow extends React.Component {
     try {
       const resCreateComment = await createComment(formData)
 
-      if (resCreateComment.status === 201 ){
+      if (resCreateComment.status === 201) {
         const photoId = this.props.match.params.id
         const response = await getSinglePhoto(photoId)
 
@@ -96,7 +99,7 @@ class PhotoShow extends React.Component {
           singlePhotoData: response.data,
           formText: ''
         })
-      } 
+      }
     } catch (err) {
       console.log(err)
     }
@@ -104,11 +107,11 @@ class PhotoShow extends React.Component {
 
   render() {
 
-    if (!this.state.singlePhotoData) return <h1>Just getting that for you</h1>
+    if (!this.state.singlePhotoData || !this.state.currentUserPhoto) return <h1>Just getting that for you</h1>
 
     const { image, title, comments } = this.state.singlePhotoData
     const { username, profile_image } = this.state.singlePhotoData.owner
-    const { heartColor, formText, postBtnHidden } = this.state
+    const { heartColor, formText, postBtnHidden, currentUserPhoto } = this.state
 
 
     return (
@@ -116,72 +119,81 @@ class PhotoShow extends React.Component {
         <div className='singlephoto-container'>
           <img src={image} />
         </div>
-        <div className='singlephoto-info-outer'>
-          <div className='singlephoto-owner-avatar'>
-            <img src={profile_image} style={{ borderRadius: '50%', width: '50px' }} />
+        <section className='singlephoto-lower-container'>
+          <div className='singlephoto-info-outer'>
+            <div className='singlephoto-owner-avatar'>
+              <img src={profile_image} style={{ borderRadius: '50%', width: '50px' }} />
+            </div>
+            <div className='heart-btn-container'>
+              <Icon
+                name='heart'
+                size='big'
+                color={heartColor}
+                onClick={this.handleClick} />
+            </div>
+
+            <h2>{title}</h2>
+            <p>by {username}</p>
+
           </div>
-          <div className='heart-btn-container'>
-            <Icon
-              name='heart'
-              size='big'
-              color={heartColor}
-              onClick={this.handleClick} />
+
+          <div className='comments-outer-container'>
+            <Comment.Group>
+
+              {isAuthenticated() &&
+                <Form >
+                  <div className='form-input-container'>
+                    <img src={currentUserPhoto} style={{ width: '30px', height: '30px', borderRadius: '50%', marginRight: '5px', marginTop: '5px' }} />
+                    <Form.Input
+                      onClick={this.handleTextBoxClick}
+                      onChange={this.handleTextChange}
+                      value={formText}
+                      placeholder={'💬   Add a comment'}
+                    />
+                  </div>
+                  <div className={`comment-btns-container ${postBtnHidden ? 'hidden-btns' : 'show-btns'}`}>
+                    <Button
+                      className='cancel-btn'
+                      onClick={this.handleCancelClick}
+                    >
+                      Cancel
+                    </Button>
+                    <Button primary
+                      className='lozenge'
+                      onClick={this.handleCommentSubmit}
+                    >
+                      Post
+                    </Button>
+                  </div>
+                </Form>
+              }
+
+              <p style={{ marginTop: '7px' }}><strong>{comments.length} comments</strong></p>
+              {!comments.length &&
+                <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+                  <Icon name='comment outline' size='big' />
+                  <br />
+                  <br />
+                  <p>No comments yet</p>
+                </div>
+              }
+              {comments.map((comment, index) => {
+                return (
+                  <Comment key={index}>
+                    <img src={comment.owner.profile_image} style={{ width: '40px', height: '40px', borderRadius: '50%' }} />
+                    <div style={{ display: 'inline-block', marginLeft: '10px' }}>
+                      <Comment.Author>{comment.owner.username}</Comment.Author>
+                      <Comment.Text>{comment.text}</Comment.Text>
+                    </div>
+                  </Comment>
+                )
+              })
+              }
+
+
+            </Comment.Group>
           </div>
-
-          <h2>{title}</h2>
-          <p>by {username}</p>
-
-        </div>
-
-        <Comment.Group>
-
-          {isAuthenticated() &&
-            <Form >
-              <Form.Input
-                onClick={this.handleTextBoxClick}
-                onChange={this.handleTextChange}
-                value={formText}
-                placeholder={'💬   Add a comment'} />
-              <div className={`comment-btns-container ${postBtnHidden ? 'hidden-btns' : 'show-btns'}`}>
-                <Button 
-                  className='cancel-btn'
-                  onClick={this.handleCancelClick}
-                >
-                  Cancel
-                </Button>
-                <Button primary
-                  className='lozenge'
-                  onClick={this.handleCommentSubmit}
-                >
-                  Post
-                </Button>
-              </div>
-            </Form>
-          }
-
-          <p>{comments.length} comments</p>
-          {!comments.length &&
-            <>
-              <Icon name='comment outline' size='big' />
-              <br />
-              <br />
-              <p>No comments yet</p>
-            </>
-          }
-          {comments.map((comment, index) => {
-            return (
-              <Comment key={index}>
-                <Comment.Avatar src={comment.owner.profile_image} />
-                <Comment.Author>{comment.owner.username}</Comment.Author>
-                <Comment.Text>{comment.text}</Comment.Text>
-              </Comment>
-            )
-          })
-          }
-
-
-        </Comment.Group>
-
+        </section>
       </>
     )
   }
