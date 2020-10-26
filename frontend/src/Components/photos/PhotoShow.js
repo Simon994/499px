f26203dde6b0533fe12fdcbaefb1cd67c9c0261d
SimonN <65023744+Simon994@users.noreply.github.com>
2020-10-26 12:38:36 +0000

    adding like/unlike to PhotoShow. Like needs refactoring as now using in multiple places

--- frontend/src/Components/photos/PhotoShow.js
+++ frontend/src/Components/photos/PhotoShow.js
@@ -1,6 +1,7 @@
 import React from 'react'
+import { Icon } from 'semantic-ui-react'
 
-import { getSinglePhoto } from '../../lib/api'
+import { getSinglePhoto, getUserProfile, likePhoto, unlikePhoto } from '../../lib/api'
 
 class PhotoShow extends React.Component {
 
@@ -12,20 +13,77 @@ class PhotoShow extends React.Component {
     const photoId = this.props.match.params.id
 
     const response = await getSinglePhoto(photoId)
-    console.log('HERE IS SINGLE PHOTO INFO', response)
+    const currentUserProfile = await getUserProfile()
+    const currentUserId = currentUserProfile.data.id
+
+    const likedByArrayIds = response.data.liked_by.map(likedBy => likedBy.id )
+    const isLikedByCurrentUser = likedByArrayIds.includes(currentUserId)
+    const heartColor = isLikedByCurrentUser ? 'pink' : 'grey'
 
     this.setState({
-      singlePhotoData: response.data
+      singlePhotoData: response.data,
+      heartColor,
+      liked: isLikedByCurrentUser
     })
   }
 
+  handleClick = async() => {
+
+    const { id } = this.props.match.params
+    console.log('GET ID', id)
+    const { liked } = this.state
+
+    if (!liked){
+      try {
+        await likePhoto(id)
+        this.setState({
+          heartColor: 'pink',
+          liked: true
+        })
+      } catch (err) {
+        console.log(err)
+      }
+    } else {
+      try {
+        await unlikePhoto(id)
+        this.setState({
+          heartColor: 'grey',
+          liked: false
+        })
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  }
 
   render(){
     
     if (!this.state.singlePhotoData) return <h1>Just getting that for you</h1>
     
+    const { image, title } = this.state.singlePhotoData
+    const { username } = this.state.singlePhotoData.owner
+    const { heartColor } = this.state
+
     return (
-      <h1>SHOWing photo</h1>
+      <>
+        <div className='singlephoto-container'>
+          <img src={image}/>
+        </div>
+        <div className='singlephoto-info-outer'>
+
+          <div className='heart-btn-container'>
+            <Icon
+              name='heart'
+              size='big'
+              color={heartColor}
+              onClick={this.handleClick} />
+          </div>
+
+          <h2>{title}</h2>
+          <p>by {username}</p>
+
+        </div>
+      </>
     )
   }
 
